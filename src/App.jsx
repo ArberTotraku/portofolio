ddf82f1797725045cda25c23fdc5360a5b4abc37
Arber Totraku <arbertotraku@gmail.com>
2023-10-17 23:32:31 +0200

    found a better hamburger menu :D

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import About from './pages/About.jsx'
-import Footer from './pages/Contact.jsx'
-import Projects from './pages/Projects.jsx'
-import "./App.css"
-import Header from './pages/Header.jsx'
-import HamburgerMenu from './components/HamburgerMenu.jsx'
+import React from "react";
+import About from "./pages/About.jsx";
+import Footer from "./pages/Contact.jsx";
+import Projects from "./pages/Projects.jsx";
+import "./App.css";
+import Header from "./pages/Header.jsx";
+import Nav from "../src/components/Hamburger.jsx";
+// import HamburgerMenu from './components/HamburgerMenu.jsx'
 // import Navbar from './pages/Navbar.jsx'
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline m-0 auto">
-      Hello world!
-    </h1>
-    {/* <Navbar/> */}
-    <HamburgerMenu/>
-<Header/>
-    <About/>
-<Projects/>
-    <Footer/>
+      <h1 className="text-3xl font-bold underline m-0 auto">Hello world!</h1>
+      {/* <Navbar/> */}
+      {/* <HamburgerMenu/> */}
+      <Nav />
+      <Header />
+      <About />
+      <Projects />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
