2e7060bf4f42703030f900e859d0d624b11dd862
Arber Totraku <arbertotraku@gmail.com>
2023-10-18 00:41:19 +0200

    ..

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -14,8 +14,8 @@ const Nav = () => {
     <div className='shadow-md w-full fixed top-0 left-0'>
       <div className='md:flex items-center justify-between bg-gray-800 py-4 md:px-10 px-7'>
       <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
-      text-gray-800'>
-        <span className='text-3xl text-indigo-600 mr-1 pt-2'>
+      text-white'>
+        <span className='text-3xl text-indigo-600 mr-1 pt-2 text-white'>
         <ion-icon name="logo-ionic"></ion-icon>
         </span>
      Portofolio
