b57229889323eca8e5707432589ae670dd34d328
Arber Totraku <arbertotraku@gmail.com>
2023-10-20 01:08:49 +0200

    fixed Cv

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,34 +1,54 @@
 import React from 'react';
-import profile from "../images/profile.jpeg"
+import profile from "../images/profile.jpeg";
 
 const About = () => {
-    return (
-        <div className="bg-gray-800 text-white py-10">
-            <div className="container mx-auto p-4 bg-gray-600">
-                <h1 className="text-3xl font-bold mb-6 text-center">About</h1>
-                <div className="bg-grey-600 p-6 rounded-lg shadow-md">
-                    <img
-                        src={profile}
-                        alt="Arber Totraku"
-                        className="w-90 h-90 rounded mx-auto mb-5"
-                    />
-                    <p className="text-white text-center mt-4">
-                        Hi, I'm Arber Totraku and I live in Berlin, Germany. I am a passionate full stack web developer with a focus on creating
-                        user-friendly and visually appealing web applications. I have a
-                        strong background in HTML, CSS, JavaScript, and modern web
-                        technologies.
-                    </p>
-                    <p className="text-white text-center mt-4">
-                        In my free time, I enjoy traveling, swimming and playing chess. So if you think that you're better then me in chess, you are absolutely right. I am also a big fan of espresso. I think that espresso has the magic to turn coffee into code.
-                    </p>
-                    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mt-3">
-  <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-  <span> <a href="./myCv.pdf" download="myCv.pdf">Download my Cv here</a></span>
-</button>
-                </div>
-            </div>
+  const onButtonClick = () => {
+    fetch('../public/myCv.pdf')
+      .then((response) => response.blob())
+      .then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let alink = document.getElementById('cv');
+        alink.href = fileURL;
+        alink.download = 'myCv.pdf';
+        alink.click();
+      });
+  };
+
+  return (
+    <div className="bg-gray-800 text-white py-10">
+      <div className="container mx-auto p-4 bg-gray-600">
+        <h1 className="text-3xl font-bold mb-6 text-center">About</h1>
+        <div className="bg-grey-600 p-6 rounded-lg shadow-md">
+          <img src={profile} alt="Arber Totraku" className="w-90 h-90 rounded mx-auto mb-5" />
+          <p className="text-white text-center mt-4">
+            Hi, I'm Arber Totraku and I live in Berlin, Germany. I am a passionate full stack web developer with a focus on creating
+            user-friendly and visually appealing web applications. I have a
+            strong background in HTML, CSS, JavaScript, and modern web technologies.
+          </p>
+          <p className="text-white text-center mt-4">
+            In my free time, I enjoy traveling, swimming and playing chess. So if you think that you're better than me in chess, you are absolutely right. I am also a big fan of espresso. I think that espresso has the magic to turn coffee into code.
+          </p>
+          <div className="text-center mt-4">
+            <button
+              onClick={onButtonClick}
+              className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mt-3"
+            >
+              <svg
+                className="fill-current w-4 h-4 mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+              </svg>
+              <span id='cv'>
+                <a href="./myCv.pdf" download="myCv.pdf">Download my Cv here</a>
+              </span>
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ComingSoon = () => {
   return (
-    <div className='text-3xl bg-gray-800 container mx-auto text-center text-white mt-10 mb-5'>
+    <div className='text-3xl bg-gray-700 container mx-auto text-center text-white mt-10 mb-5 p-20'>
         <h1 className='text-center'>Coming Soon interaction with AI</h1>
         <p>Here will be the AI assistant which is called Arberbot, nice name right?</p>
         </div>
