2de13541e0c04469d7e7f6a66126d4da2c3ed146
Arber Totraku <arbertotraku@gmail.com>
2023-10-29 01:54:42 +0200

    small modifications and it will be ready soon

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,9 @@ import About from "./pages/About.jsx";
 import Footer from "./pages/Footer.jsx";
 import Projects from "./pages/Projects.jsx";
 import "./App.css";
-// import Header from "./pages/Header.jsx";
 import Nav from "../src/components/Hamburger.jsx";
-import Contact from "./pages/Contact.jsx"
-// import Blog from "./pages/Blog.jsx"
-// import ChatComponent from "./components/ChatComponent.jsx";
-// import { Route , Routes} from "react-router-dom";
-// import Layout from "./pages/Layout.jsx";
-// import Home from "./pages/Home.jsx";
-// import HamburgerMenu from './components/HamburgerMenu.jsx'
-// import Navbar from './pages/Navbar.jsx'
-import Services from "./pages/Services.jsx"
+import Contact from "./pages/Contact.jsx";
+import Services from "./pages/Services.jsx";
 import TestimonialsPage from "./pages/TestimonialsPage.jsx";
 import ScrollToTopButton from "./utils/ScrollToTopButton.jsx";
 
@@ -21,30 +13,17 @@ const App = () => {
   return (
     <div className="bg-gray-800 text-white">
       <h1 className="text-3xl font-bold underline m-0 auto">Hello world!</h1>
-      {/* <Navbar/> */}
-      {/* <HamburgerMenu/> */}
-      <Nav/>
-      {/* <Header /> */}
+
+      <Nav />
+
       <About />
       <Projects />
-      {/* <ChatComponent/> */}
-<Services/>
-      <TestimonialsPage/>
-      <Contact/>
-      <ScrollToTopButton/>
-      <Footer />
-     
-{/* 
-      <Routes>
-        <Route path="/" element={<Layout />} >
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="blog" element={<Blog />} />
 
-</Route>
-      </Routes> */}
+      <Services />
+      <TestimonialsPage />
+      <Contact />
+      <ScrollToTopButton />
+      <Footer />
     </div>
   );
 };
--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,61 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import Button from './Button.jsx';
 
 const Nav = () => {
-  const Links = [
-    { name: "HOME", link: "/#about" },
-    { name: "SERVICE", link: "#services" },
-    { name: "ABOUT", link: "#about" },
-    { name: "TESTIMONIALS", link: "#testimonials" },
-    { name: "CONTACT", link: "#contact" },
-  ];
-  const [open, setOpen] = useState(false);
+    let Links =[
+      {name:"HOME",link:"#about"},
+      {name:"SERVICE",link:"/#services"},
+      {name:"ABOUT",link:"#about"},
+      {name:"TESTIMONIALS",link:"#testimonials"},
+      {name:"CONTACT",link:"#contact"},
+    ];
+    let [open,setOpen]=useState(false);
 
-  const scrollToSection = (id) => {
-    const target = document.querySelector(id);
-    if (target) {
-      window.scrollTo({
-        top: target.offsetTop - 70, // Adjust the offset as needed
-        behavior: 'smooth',
-      });
-      setOpen(false)
+    const scrollToSection = (id) => {
+      const target = document.querySelector(id);
+      if (target) {
+        window.scrollTo( {
+          top:target.offsetTop - 70,
+          behavior:'smooth'
+        } )
+        setOpen(false)
+      }
     }
-  };
-
   return (
-    <div className="shadow-md w-full fixed top-0 left-0">
-      <div className="md:flex items-center justify-between bg-gray-800 py-4 md:px-10 px-7">
-        <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-white">
-          <span className="text-3xl text-indigo-600 mr-1 pt-2 text-white">
-            <ion-icon name="logo-ionic"></ion-icon>
-          </span>
-          Portofolio
-        </div>
+    <div className='shadow-md w-full fixed top-0 left-0'>
+      <div className='md:flex items-center justify-between bg-gray-800 py-4 md:px-10 px-7'>
+      <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-white'>
+        <span className='text-3xl text-indigo-600 mr-1 pt-2 text-white'>
+        <ion-icon name="logo-ionic"></ion-icon>
+        </span>
+     Portofolio
+      </div>
+      
+      <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+      <ion-icon name={open ? 'close':'menu'}></ion-icon>
+      </div>
 
-        <div onClick={() => setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
-          <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
-        </div>
-
-        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-gray-800 text-white md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20' : 'top-[-490px]'}`}>
-          {Links.map((link) => (
-            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
-              <a
-                href={link.link}
-                className="text-gray-800 hover:text-gray-400 duration-500 text-white"
-                onClick={(e) => {
-                  e.preventDefault();
-                  scrollToSection(link.link); // Scroll to the target section
-                }}
-              >
-                {link.name}
-              </a>
+      <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-gray-800 text-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+        {
+          Links.map((link)=>(
+            <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+           
+              <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500 text-white' onClick={(e) => {
+                e.preventDefault();
+                scrollToSection(link.link)
+              }  } >{link.name}</a>
             </li>
-          ))}
-          <Button>Get Started</Button>
-        </ul>
+          ))
+        }
+        <Button>
+          Get Started
+        </Button>
+      </ul>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Nav;
+export default Nav
