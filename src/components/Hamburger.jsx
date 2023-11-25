1b1b80c13f12328ecb793a942ae2e48f3c2ea9e2
Arber Totraku <arbertotraku@gmail.com>
2023-11-26 00:19:37 +0100

    a website is like a private house, It never finishes

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -5,7 +5,7 @@ import Button from './Button.jsx';
 
 const Nav = () => {
     let Links =[
-      {name:"HOME",link:"#about"},
+  
       {name:"SERVICE",link:"#services"},
       {name:"ABOUT",link:"#about"},
       {name:"TESTIMONIALS",link:"#testimonials"},
